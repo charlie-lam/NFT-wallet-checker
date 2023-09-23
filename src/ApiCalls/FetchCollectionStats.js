06144c9651af5d7b87d76a0c04350919fee5d4a2
Charlie Lambert <93406456+charlie-lam@users.noreply.github.com>
2023-09-23 14:38:41 +0100

    new fetch call for collection stats

---
+++ src/ApiCalls/FetchCollectionStats.js
@@ -0,0 +1,10 @@
+export const fetchCollectionStats = async (slug) => {
+    const options = {
+        method: 'GET',
+        headers: {accept: 'application/json', 'X-API-KEY': `${process.env.REACT_APP_OPENSEA_API_KEY}`}
+      };
+      
+    const response = await fetch(`https://api.opensea.io/api/v1/collection/${slug}/stats`, options);
+    const stats = await response.json();
+    return stats.stats;
+}
