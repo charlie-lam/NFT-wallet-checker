de496f8a9669c2a4d092f2b523b2533784118324
Charlie Lambert <93406456+charlie-lam@users.noreply.github.com>
2023-09-23 14:31:14 +0100

    add missing await on fetch eth price call

--- src/ApiCalls/FetchEthPrice.js
+++ src/ApiCalls/FetchEthPrice.js
@@ -1,6 +1,6 @@
 const FetchEthPrice = async () => {
     const response = await fetch(`https://rest.coinapi.io/v1/exchangerate/ETH/USD?apikey=${process.env.REACT_APP_COIN_API_KEY}`);
-    const data = response.json();
+    const data = await response.json();
     const price = data.rate.toFixed(2);
     return price;
 }
