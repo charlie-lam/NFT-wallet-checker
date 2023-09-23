6162ca7649baf7995353f340546b50f2e4817ead
Charlie Lambert <93406456+charlie-lam@users.noreply.github.com>
2023-09-23 23:59:55 +0100

    Fill out wallet summary

--- src/components/WalletSummary.js
+++ src/components/WalletSummary.js
@@ -3,12 +3,19 @@ import { useEffect, useState } from "react";
 export const WalletSummary = ({walletAddress, collections, ethPrice}) => {
 
 
-    const [totalNfts, setTotalNfts] = useState(null);
+    const [totalNfts, setTotalNfts] = useState(0);
+    const [liquidNfts, setLiquidNfts] = useState(0);
+    const [ethValue, setEthValue] = useState(0);
+    const [dollarValue, setDollarValue] = useState(0);
+
     useEffect(() => {
-            setTotalNfts(collections.map(collection => collection.nfts).reduce((acc, collectionNfts) => acc + collectionNfts.length, 0))
+        setTotalNfts(collections.map(collection => collection.nfts).reduce((acc, collectionNfts) => acc + collectionNfts.length, 0));
+        const filteredCollections = collections.filter(collection => collection.collectionStats["seven_day_volume"] > 0.01);
+        setLiquidNfts(filteredCollections.map(collection => collection.nfts).reduce((acc, collectionNfts) => acc + collectionNfts.length, 0));
+        setEthValue(filteredCollections.map(collection => collection.collectionStats["floor_price"]).reduce((acc, floor) => acc + floor));
+        setDollarValue(ethValue*ethPrice);
     })
 
-    if(collections){
     return ( 
             <div id="wallet-summary">
                 <div id="address-section" className="sum-div">
@@ -19,9 +26,9 @@ export const WalletSummary = ({walletAddress, collections, ethPrice}) => {
                 <p id="total-label">Total NFTs</p>
                 <p if="total-nfts">{totalNfts}</p>
                 </div>
-                {/* <div id="non-zero" className="sum-div">
+                <div id="non-zero" className="sum-div">
                 <p id="liquid-label">Liquid NFTs</p>
-                <p id="liquid-nfts">{liquid}</p>
+                <p id="liquid-nfts">{liquidNfts}</p>
                 </div>
                 <div id="eth-value" className="sum-div">
                 <p id="eth-label">NFT Eth Value</p>
@@ -30,8 +37,7 @@ export const WalletSummary = ({walletAddress, collections, ethPrice}) => {
                 <div id="dollar-value" className="sum-div">
                 <p id="dollar-label">NFT Dollar Value</p>
                 <p id="dollar-total">{dollarValue.toFixed(2)}</p>
-                </div> */}
+                </div>
             </div>
         );
-    }
 }
