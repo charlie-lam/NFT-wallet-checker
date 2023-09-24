e293d4367b95fe70e13f658c43ae514a654eea3f
Charlie Lambert <93406456+charlie-lam@users.noreply.github.com>
2023-09-24 18:48:33 +0100

    prettier WalletSummary and Form

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,41 +1,40 @@
 import { useState } from "react";
 
-export const WalletForm = ({walletAddress, handleWalletSubmit}) => {
-    
-    const [typed, setTyped] = useState("")
+export const WalletForm = ({ walletAddress, handleWalletSubmit }) => {
+  const [typed, setTyped] = useState("");
 
-    const onChange = (event) => {
-      setTyped(event.target.value)
-    }
+  const onChange = (event) => {
+    setTyped(event.target.value);
+  };
 
-    const onSubmit = (event) => {
-      event.preventDefault();
-      handleWalletSubmit(typed);
-    };
-    
-    let styleFull = {
-      width: "20%",
-      display: "flex",
-      flexDirection: "column",
-      padding: 10,
-      borderRadius: 10,
-    };
-    let defaultStyle;
-    return (
-      <div id="form-div" style={walletAddress !== "" ? styleFull : defaultStyle}>
-        <form id="wallet-ask" onSubmit={onSubmit}>
-          <label htmlFor="wallet-input">Eth wallet:</label>
-          <br />
-          <input
-            type="text"
-            name="wallet-input "
-            id="wallet-input"
-            placeholder="Enter here"
-            value={typed}
-            onChange={onChange}
-          ></input>
-          <input id="wallet-submit" type="submit" value="Submit" />
-        </form>
-      </div>
-    );
-  }
+  const onSubmit = (event) => {
+    event.preventDefault();
+    handleWalletSubmit(typed);
+  };
+
+  let styleFull = {
+    width: "20%",
+    display: "flex",
+    flexDirection: "column",
+    padding: 10,
+    borderRadius: 10,
+  };
+  let defaultStyle;
+  return (
+    <div id="form-div" style={walletAddress !== "" ? styleFull : defaultStyle}>
+      <form id="wallet-ask" onSubmit={onSubmit}>
+        <label htmlFor="wallet-input">Eth wallet:</label>
+        <br />
+        <input
+          type="text"
+          name="wallet-input "
+          id="wallet-input"
+          placeholder="Enter here"
+          value={typed}
+          onChange={onChange}
+        ></input>
+        <input id="wallet-submit" type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
--- src/components/WalletSummary.js
+++ src/components/WalletSummary.js
@@ -1,43 +1,55 @@
 import { useEffect, useState } from "react";
 
-export const WalletSummary = ({walletAddress, collections, ethPrice}) => {
+export const WalletSummary = ({ walletAddress, collections, ethPrice }) => {
+  const [totalNfts, setTotalNfts] = useState(0);
+  const [liquidNfts, setLiquidNfts] = useState(0);
+  const [ethValue, setEthValue] = useState(0);
+  const [dollarValue, setDollarValue] = useState(0);
 
+  useEffect(() => {
+    setTotalNfts(
+      collections
+        .map((collection) => collection.nfts)
+        .reduce((acc, collectionNfts) => acc + collectionNfts.length, 0),
+    );
+    const filteredCollections = collections.filter(
+      (collection) => collection.sevenDayVolume > 0.01,
+    );
+    setLiquidNfts(
+      filteredCollections
+        .map((collection) => collection.nfts)
+        .reduce((acc, collectionNfts) => acc + collectionNfts.length, 0),
+    );
+    setEthValue(
+      filteredCollections
+        .map((collection) => collection.floorPrice)
+        .reduce((acc, floor) => acc + floor),
+    );
+    setDollarValue(ethValue * ethPrice);
+  });
 
-    const [totalNfts, setTotalNfts] = useState(0);
-    const [liquidNfts, setLiquidNfts] = useState(0);
-    const [ethValue, setEthValue] = useState(0);
-    const [dollarValue, setDollarValue] = useState(0);
-
-    useEffect(() => {
-        setTotalNfts(collections.map(collection => collection.nfts).reduce((acc, collectionNfts) => acc + collectionNfts.length, 0));
-        const filteredCollections = collections.filter(collection => collection.sevenDayVolume > 0.01);
-        setLiquidNfts(filteredCollections.map(collection => collection.nfts).reduce((acc, collectionNfts) => acc + collectionNfts.length, 0));
-        setEthValue(filteredCollections.map(collection => collection.floorPrice).reduce((acc, floor) => acc + floor));
-        setDollarValue(ethValue*ethPrice);
-    })
-
-    return ( 
-            <div id="wallet-summary">
-                <div id="address-section" className="sum-div">
-                <p id="wallet-label">Eth Wallet</p>
-                <p id="wallet-id">{walletAddress}</p>
-                </div>
-                <div id="all-nfts" className="sum-div">
-                <p id="total-label">Total NFTs</p>
-                <p if="total-nfts">{totalNfts}</p>
-                </div>
-                <div id="non-zero" className="sum-div">
-                <p id="liquid-label">Liquid NFTs</p>
-                <p id="liquid-nfts">{liquidNfts}</p>
-                </div>
-                <div id="eth-value" className="sum-div">
-                <p id="eth-label">NFT Eth Value</p>
-                <p id="eth-total">{ethValue}</p>
-                </div>
-                <div id="dollar-value" className="sum-div">
-                <p id="dollar-label">NFT Dollar Value</p>
-                <p id="dollar-total">{dollarValue.toFixed(2)}</p>
-                </div>
-            </div>
-        );
-}
+  return (
+    <div id="wallet-summary">
+      <div id="address-section" className="sum-div">
+        <p id="wallet-label">Eth Wallet</p>
+        <p id="wallet-id">{walletAddress}</p>
+      </div>
+      <div id="all-nfts" className="sum-div">
+        <p id="total-label">Total NFTs</p>
+        <p if="total-nfts">{totalNfts}</p>
+      </div>
+      <div id="non-zero" className="sum-div">
+        <p id="liquid-label">Liquid NFTs</p>
+        <p id="liquid-nfts">{liquidNfts}</p>
+      </div>
+      <div id="eth-value" className="sum-div">
+        <p id="eth-label">NFT Eth Value</p>
+        <p id="eth-total">{ethValue}</p>
+      </div>
+      <div id="dollar-value" className="sum-div">
+        <p id="dollar-label">NFT Dollar Value</p>
+        <p id="dollar-total">{dollarValue.toFixed(2)}</p>
+      </div>
+    </div>
+  );
+};
