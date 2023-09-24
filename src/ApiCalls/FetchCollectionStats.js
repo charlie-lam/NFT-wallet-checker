09909534fb3def74ce424786dfe0466de23be697
Charlie Lambert <93406456+charlie-lam@users.noreply.github.com>
2023-09-24 18:47:30 +0100

    Prettier fetch files

--- src/ApiCalls/FetchCollectionStats.js
+++ src/ApiCalls/FetchCollectionStats.js
@@ -1,10 +1,16 @@
 export const fetchCollectionStats = async (slug) => {
-    const options = {
-        method: 'GET',
-        headers: {accept: 'application/json', 'X-API-KEY': `${process.env.REACT_APP_OPENSEA_API_KEY}`}
-      };
-      
-    const response = await fetch(`https://api.opensea.io/api/v1/collection/${slug}/stats`, options);
-    const stats = await response.json();
-    return stats.stats;
-}
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      "X-API-KEY": `${process.env.REACT_APP_OPENSEA_API_KEY}`,
+    },
+  };
+
+  const response = await fetch(
+    `https://api.opensea.io/api/v1/collection/${slug}/stats`,
+    options,
+  );
+  const stats = await response.json();
+  return stats.stats;
+};
--- src/ApiCalls/FetchNfts.js
+++ src/ApiCalls/FetchNfts.js
@@ -1,10 +1,16 @@
 export const fetchNfts = async (address) => {
-    const options = {
-        method: 'GET',
-        headers: {accept: 'application/json', 'X-API-KEY': `${process.env.REACT_APP_OPENSEA_API_KEY}`}
-      };
-      
-      const response = await fetch(`https://api.opensea.io/v2/chain/ethereum/account/${address}/nfts`, options)
-      const nfts = await response.json();
-      return nfts.nfts;
-}
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      "X-API-KEY": `${process.env.REACT_APP_OPENSEA_API_KEY}`,
+    },
+  };
+
+  const response = await fetch(
+    `https://api.opensea.io/v2/chain/ethereum/account/${address}/nfts`,
+    options,
+  );
+  const nfts = await response.json();
+  return nfts.nfts;
+};
