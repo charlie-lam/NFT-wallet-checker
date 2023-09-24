4147bdc9bdedfa21d5bde01f1e2d7fbf3fe2b8c2
Charlie Lambert <93406456+charlie-lam@users.noreply.github.com>
2023-09-24 18:49:50 +0100

    Finish app with collection component and pretty

--- src/App.js
+++ src/App.js
@@ -1,115 +1,120 @@
 import { fetchEthPrice } from "./ApiCalls/FetchEthPrice";
-import {fetchNfts} from "./ApiCalls/FetchNfts";
+import { fetchNfts } from "./ApiCalls/FetchNfts";
 import { fetchCollectionStats } from "./ApiCalls/FetchCollectionStats";
-import {WalletForm} from "./components/WalletForm";
-import {WalletSummary} from "./components/WalletSummary";
+import { WalletForm } from "./components/WalletForm";
+import { WalletSummary } from "./components/WalletSummary";
+import { Collections } from "./components/Collections";
 import { useState, useEffect } from "react";
-import "./App.css"
+import "./App.css";
 
 const App = () => {
+  const [ethPrice, setEthPrice] = useState("");
+  const [priceError, setPriceError] = useState(false);
 
-    const [ethPrice, setEthPrice] = useState("");
-    const [priceError, setPriceError] = useState(false);
+  const ReturnEthPrice = async () => {
+    try {
+      const price = await fetchEthPrice();
+      setEthPrice(price);
+    } catch (error) {
+      console.log(error);
+      setPriceError(true);
+    }
+  };
 
-    const ReturnEthPrice = async () => {
-        try{
-            const price = await fetchEthPrice();
-            setEthPrice(price);
-        }
-        catch (error){
-            console.log(error);
-            setPriceError(true);
+  useEffect(() => {
+    ReturnEthPrice();
+  }, []);
+
+  const [walletAddress, setWalletAddress] = useState("");
+  const [collections, setCollections] = useState(null);
+  const [nftCallError, setNftCallError] = useState(false);
+
+  const returnNfts = async (address) => {
+    try {
+      return await fetchNfts(address);
+    } catch (error) {
+      setNftCallError(true);
+    }
+  };
+
+  const groupNfts = (nftArray) => {
+    return nftArray.reduce((acc, nft) => {
+      const { collection, ...rest } = nft;
+      if (!acc[collection]) {
+        acc[collection] = [];
+      }
+      acc[collection].push(rest);
+      return acc;
+    }, {});
+  };
+
+  const returnCollectionStats = async (collectionName) => {
+    try {
+      const stats = await fetchCollectionStats(collectionName);
+      const sevenDayVolume = stats["seven_day_volume"]
+        ? stats["seven_day_volume"]
+        : 0;
+      const floorPrice = stats["floor_price"] ? stats["floor_price"] : 0;
+      return { sevenDayVolume, floorPrice };
+    } catch (error) {
+      return { sevenDayVolume: 0, floorPrice: 0 };
+    }
+  };
+
+  const fillCollectionsStats = async (partialCollections) => {
+    const statCompleteCollections = await Promise.all(
+      partialCollections.map(async (collection) => {
+        const collectionStats = await returnCollectionStats(collection.name);
+        return {
+          ...collection,
+          sevenDayVolume: collectionStats.sevenDayVolume,
+          floorPrice: collectionStats.floorPrice,
         };
-    };
+      }),
+    );
+    return statCompleteCollections;
+  };
 
-    // useEffect(() => {
-    //   ReturnEthPrice();
-    // }, []);
+  const handleWalletSubmit = async (address) => {
+    setWalletAddress(address);
+    const returnedNfts = await returnNfts(address);
+    const groupedNftObject = groupNfts(returnedNfts);
+    const partialCollectionsArray = Object.entries(groupedNftObject).map(
+      ([name, nfts]) => ({
+        name,
+        nfts,
+      }),
+    );
+    const statFilledCollections = await fillCollectionsStats(
+      partialCollectionsArray,
+    );
+    setCollections(statFilledCollections);
+  };
 
-    const [walletAddress, setWalletAddress] = useState("");
-    const [collections, setCollections] = useState(null);
-    const [nftCallError, setNftCallError] = useState(false);
-
-    const ReturnNfts = async (address) => {
-        try{
-            return await fetchNfts(address);
-        }
-        catch (error){
-            setNftCallError(true);
-        }
-    };
-
-    const groupNfts = (nftArray) => {
-      return nftArray.reduce((acc, nft) => {
-        const {collection, ...rest} = nft;
-        if(!acc[collection]){
-          acc[collection] = [];
-        }
-        acc[collection].push(rest)
-        return acc;
-      }, {});
-    }
-
-    const ReturnCollectionStats = async (collectionName) => {
-      try{
-        const stats = await fetchCollectionStats(collectionName);
-        const sevenDayVolume = stats["seven_day_volume"] ? stats["seven_day_volume"] : 0;
-        const floorPrice = stats["floor_price"] ? stats["floor_price"] : 0;
-        return {sevenDayVolume: sevenDayVolume, floorPrice: floorPrice};
-    }
-    catch (error){
-        return {sevenDayVolume : 0, floorPrice : 0};
-    };
-    };
-
-    const fillCollectionsStats = async (partialCollections) => {
-      const statCompleteCollections = await Promise.all(
-        partialCollections.map( async (collection) => {
-          const collectionStats = await ReturnCollectionStats(collection.name)
-          return {...collection, sevenDayVolume : collectionStats.sevenDayVolume, floorPrice : collectionStats.floorPrice}
-        })
-      )
-      return statCompleteCollections;
-    };
-
-    const handleWalletSubmit = async (address) => {
-        setWalletAddress(address);
-        const returnedNfts = await ReturnNfts(address);
-        const groupedNftObject = groupNfts(returnedNfts);
-        const partialCollectionsArray = Object.entries(groupedNftObject).map(([name, nfts]) => ({
-          name,
-          nfts,
-        }))
-        const statFilledCollections = await fillCollectionsStats(partialCollectionsArray);
-        setCollections(statFilledCollections);
-    };
-
-    return (
-        <div id="whole">
-          <header>
-            <h2>NFT Wallet Checker</h2>
-            <h4>Eth price:${ethPrice}</h4>
-            {priceError && <h4>Eth price unavailable</h4>}
-          </header>
-          <div id="content">
-            <WalletForm
-              handleWalletSubmit={handleWalletSubmit}
-              walletAddress={walletAddress}
-            />
-            {collections && (
-              <WalletSummary
-                walletAddress={walletAddress}
-                collections={collections}
-                ethPrice={ethPrice}
-              />
-            )}
-            {nftCallError && <h4>Wallet unavailable</h4>}
-            {/* {walletAddress !== "" && (
-              <WalletNFTs completed={this.state.collections} />
-            )} */}
-          </div>
-        </div>
-      );
-}
+  return (
+    <div id="whole">
+      <header>
+        <h2>NFT Wallet Checker</h2>
+        <h4>Eth price:${ethPrice}</h4>
+        {priceError && <h4>Eth price unavailable</h4>}
+      </header>
+      <div id="content">
+        <WalletForm
+          handleWalletSubmit={handleWalletSubmit}
+          walletAddress={walletAddress}
+        />
+        {collections && (
+          <WalletSummary
+            walletAddress={walletAddress}
+            collections={collections}
+            ethPrice={ethPrice}
+          />
+        )}
+        {nftCallError && <h4>Wallet unavailable</h4>}
+        {collections && <Collections collections={collections} />}
+      </div>
+    </div>
+  );
+};
 
 export default App;
